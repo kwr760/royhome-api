7bab6ea45f5f508956610cb1abe2c4c46ea4a3f8
kwr760 <kroy760@gmail.com>
2021-04-23 11:23:17 -0700

    Add touch file, change cert location

--- src/main/kotlin/net/royhome/api/Application.kt
+++ src/main/kotlin/net/royhome/api/Application.kt
@@ -1,8 +1,12 @@
 package net.royhome.api
 
+import net.royhome.api.constant.Constant
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import java.io.File
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -11,4 +15,7 @@ class Application
 @Suppress("SpreadOperator")
 fun main(args: Array<String>) {
   runApplication<Application>(*args)
+  val startFile = File(Constant.STARTED_FILENAME)
+  val timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+  startFile.writeText(timestamp + System.lineSeparator())
 }
---
+++ src/main/kotlin/net/royhome/api/constant/Constant.kt
@@ -0,0 +1,5 @@
+package net.royhome.api.constant
+
+object Constant {
+  const val STARTED_FILENAME = "started"
+}
