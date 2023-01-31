30e590af5e3bd2b7c5b665ac1746c55efd2e5cfc
kwr760 <kroy760@gmail.com>
2023-01-30 17:19:54 -0800

    TTT-26 remove old game

---
+++ src/main/kotlin/net/royhome/tictactoe/service/MaintenanceService.kt
@@ -0,0 +1,20 @@
+package net.royhome.tictactoe.service
+
+import net.royhome.tictactoe.repository.GameRepository
+import net.royhome.tictactoe.repository.PlayerRepository
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class MaintenanceService(
+  val gameRepository: GameRepository,
+  val playerRepository: PlayerRepository,
+) {
+  @Transactional
+  @Scheduled(cron = "0 0/5 * * * *")
+  fun removeOldGames() {
+    playerRepository.deleteWhenAnHourOld()
+    gameRepository.deleteWhenAnHourOld()
+  }
+}
