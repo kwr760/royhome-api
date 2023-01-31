870007f30f78b35bc145e7f3ee29ac5090704758
kwr760 <kroy760@gmail.com>
2023-01-30 17:30:46 -0800

    TTT-32 Find the earliest game needs to the earliest open game

--- src/main/kotlin/net/royhome/tictactoe/repository/GameRepository.kt
+++ src/main/kotlin/net/royhome/tictactoe/repository/GameRepository.kt
@@ -12,7 +12,8 @@ interface GameRepository : JpaRepository<Game, Long> {
   fun findByGameId(gameId: UUID): Game
 
   @Query(
-    "SELECT game.* FROM tictactoe.game GROUP BY game.game_id HAVING MIN(game.created) = game.created",
+    "SELECT game.* FROM tictactoe.game WHERE game.state = 0" +
+      " GROUP BY game.game_id HAVING MIN(game.created) = game.created",
     nativeQuery = true
   )
   fun findByEarliestState(state: Int): Game?
