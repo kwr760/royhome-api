f13da374f66e84735506dc9a8ceebfe666e9f43c
kwr760 <kroy760@gmail.com>
2023-02-12 15:56:27 -0800

    WEB-201 Update yarn dep man

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ java.targetCompatibility = JavaVersion.VERSION_11
 
 plugins {
   id("org.springframework.boot") version "2.4.2"
-  id("org.flywaydb.flyway") version "7.5.4"
-  id("io.spring.dependency-management") version "1.0.11.RELEASE"
+  id("org.flywaydb.flyway") version "9.14.1"
+  id("io.spring.dependency-management") version "1.1.0"
   id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
   id("io.gitlab.arturbosch.detekt") version "1.16.0"
   id("jacoco")
